06432464bb7bf15100f2c6c5f2e526298df26919
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-09-26 20:37:11 -0400

    improved display

--- assets/JavaScript/quizscript.js
+++ assets/JavaScript/quizscript.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -81,13 +82,15 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionIndex = Math.floor(Math.random() * availableQuesions.length);
   currentQuestion = availableQuesions[questionIndex];
   question.innerText = currentQuestion.question;
 
-  choices.forEach((choice) => {
+  choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
@@ -96,8 +99,8 @@ getNewQuestion = () => {
   acceptingAnswers = true;
 };
 
-choices.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
     acceptingAnswers = false;
@@ -120,7 +123,7 @@ choices.forEach((choice) => {
   });
 });
 
-incrementScore = (num) => {
+incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
